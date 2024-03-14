2b039b559af23ec5a11a81afdcd07ce7ca062680
Daniel Dakhno <dakhnod@gmail.com>
2024-03-14 02:35:10 +0100

    allow for starting activities

--- app/src/main/java/d/d/fcmgateway/service/MessageReceiverService.kt
+++ app/src/main/java/d/d/fcmgateway/service/MessageReceiverService.kt
@@ -1,6 +1,7 @@
 package d.d.fcmgateway.service
 
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import androidx.annotation.NonNull
 
@@ -18,34 +19,58 @@ class MessageReceiverService : FirebaseMessagingService() {
         val type = data.withDefault { "intent" }["type"]
         val action = data["action"]
         val application = data["application"]
+        val applicationClass = data["class"]
         val extrasSerialized = data["extras"]
+        val uri = data["uri"]
 
-        if(listOf("broadcast").contains(type)){
+        if(listOf("broadcast", "activity").contains(type)){
             val intent = Intent()
             action?.let { intent.action = action }
             application?.let { intent.`package` = application }
+            uri?.let { intent.data = Uri.parse(uri) }
             extrasSerialized?.let {
                 val extras = JSONObject(extrasSerialized)
                 extras.keys().forEach {
-                    when(val value = extras[it]){
+                    when (val value = extras[it]) {
                         is Int -> intent.putExtra(it, value)
                         is Double -> intent.putExtra(it, value)
                         is Boolean -> intent.putExtra(it, value)
                         is String -> intent.putExtra(it, value)
-                        else -> Log.e(TAG, "onMessageReceived: key $it contains invalid class: ${value.javaClass}")
+                        else -> Log.e(
+                            TAG,
+                            "onMessageReceived: key $it contains invalid class: ${value.javaClass}"
+                        )
                     }
                 }
             }
             Log.d(TAG, "onMessageReceived: sending $type")
             when(type){
                 "broadcast" -> sendBroadcast(intent)
+                "activity" -> {
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    if(application != null){
+                        if(applicationClass == null) {
+                            val launchIntent = packageManager.getLaunchIntentForPackage(application)
+                            if (launchIntent == null) {
+                                Log.e(
+                                    TAG,
+                                    "onMessageReceived: Could not find launch intent for app '$application'"
+                                )
+                                return
+                            }
+
+                            uri?.let { intent.data = Uri.parse(uri) }
+                            startActivity(launchIntent)
+                            return
+                        }else{
+                            intent.setClassName(application, applicationClass)
+                        }
+                    }
+                    // use launchIntent here!!
+                    uri?.let { intent.data = Uri.parse(uri) }
+                    startActivity(intent)
+                }
             }
         }
-
-        Log.d(TAG, "onMessageReceived: ")
-    }
-
-    override fun onNewToken(p0: String) {
-        super.onNewToken(p0)
     }
 }
